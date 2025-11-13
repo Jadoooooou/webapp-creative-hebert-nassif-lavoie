59718ec7049c60cacd6e17004b74189bb51ee98f
Felix-Lavoie <aragonaze@gmail.com>
2025-11-13 14:40:00 -0500

    btn component

--- Un Ami pour Toujours/src/App.vue
+++ Un Ami pour Toujours/src/App.vue
@@ -1,5 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+// btn
+import BaseButton from './components/common/BaseButton.vue'
+const handleClick = () => {
+  alert('pout pout')
+}
 </script>
 
 <template>
@@ -10,6 +16,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+  </div>
+  <div>
+    <BaseButton @click="handleClick">Bouton</BaseButton>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
--- Un Ami pour Toujours/src/components/common/BaseButton.vue
+++ Un Ami pour Toujours/src/components/common/BaseButton.vue
@@ -1,11 +1,29 @@
-<template> 
+<template>
+    <button class='base-button'></button>
+    <p class="txtBtn">button</p>
+  </template>
+  
+  <script>
 
-</template>
+  </script>
+  
+  <style>
+  .base-button {
+    background-image: url(../../assets/img_btn.png);
+    background-repeat: no-repeat;
+    background-size: 100px;
+    width: 100px;
+    height: 100px;
+  }
+  
+  .base-button:hover {
+    background-color: #2563eb;
+  }
 
-<script>
-
-</script>
-
-<style>
-
-</style>
+  .txtBtn {
+    font-size: 20px;
+    margin-top: 0px;
+    margin-left: 25px;
+  }
+  
+  </style>
