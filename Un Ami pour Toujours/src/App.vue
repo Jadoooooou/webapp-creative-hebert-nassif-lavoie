a96f13e25be2af145542295d961dfd08cc6be17d
Felix-Lavoie <aragonaze@gmail.com>
2025-11-14 12:07:49 -0500

    en plein millieux de composante footer

--- Un Ami pour Toujours/src/App.vue
+++ Un Ami pour Toujours/src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
 import HomeView from './views/HomeView.vue';
-
+import AppFooter from './components/layout/AppFooter.vue'
 
 </script>
 
 <template>
   <HomeView> </HomeView>
+  <AppFooter></AppFooter>
 </template>
 
 <style scoped>
-  
+
 </style>
---
+++ Un Ami pour Toujours/src/components/layout/AppFooter.vue
@@ -0,0 +1,29 @@
+<template> 
+    <div class="bar">
+        <button class="btn">Restart</button>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    .bar {
+        background-color: #C1CEBE;
+        height: 70px;
+    }
+
+    .btn {
+        margin-top: 10px;
+        margin-left: 20px;
+        width: 200px;
+        height: 50px;
+        border: 2px;
+        color: #BFBFBF;
+    }
+
+    .btn:hover {
+        color: #9c9c9c;
+    }
+</style>
