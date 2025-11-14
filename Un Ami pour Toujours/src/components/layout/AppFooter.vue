3f7aed4906bb95b105e5a6eab92abbc3b828fb55
Felix-Lavoie <aragonaze@gmail.com>
2025-11-14 12:26:13 -0500

    update skeleton again

--- Un Ami pour Toujours/src/components/layout/AppFooter.vue
+++ Un Ami pour Toujours/src/components/layout/AppFooter.vue
@@ -1,29 +1,45 @@
 <template> 
     <div class="bar">
-        <button class="btn">Restart</button>
+        <button @click="clickBtn" class="btn">Restart</button>
     </div>
 </template>
 
-<script>
-    
+<script setup>
+    const clickBtn = () => {
+        alert('ribbit ribbit')
+    }
 </script>
 
 <style scoped>
     .bar {
         background-color: #C1CEBE;
-        height: 70px;
+        height: 60px;
+        position: fixed;   
+        bottom: 0;          
+        left: 0;
+        width: 100%;
     }
 
     .btn {
+        background-color: #c0c0c0;
+        border: 2px solid #fff; /* Top & left light border */
+        border-right-color: #808080; /* Right dark border */
+        border-bottom-color: #808080; /* Bottom dark border */
+        padding: 4px 12px;
+        font-size: 14px;
+        color: black;
+        cursor: pointer;
+        outline: none;
+        width: 200px;
+        height: 40px;
         margin-top: 10px;
         margin-left: 20px;
-        width: 200px;
-        height: 50px;
-        border: 2px;
-        color: #BFBFBF;
     }
 
     .btn:hover {
-        color: #9c9c9c;
+        border: 2px solid #808080; /* Top & left dark border */
+        border-right-color: #fff;  /* Right light border */
+        border-bottom-color: #fff; /* Bottom light border */
+        background-color: #c0c0c0;
     }
 </style>
