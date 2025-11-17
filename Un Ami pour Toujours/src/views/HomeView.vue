9b8dfecb70fe7b6050301198794dde3778c596cf
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-11-17 17:27:41 -0500

    HomeView_responsive

--- Un Ami pour Toujours/src/views/HomeView.vue
+++ Un Ami pour Toujours/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="page">
     <h1>UN AMI POUR LA VIE</h1>
     <div class="line"></div>
-    <h2>Un jeu par: Les Quenouilles</h2>
+    <h2>Un jeu par : Les Quenouilles</h2>
     <button @click="jouer" class="btnJouer">JOUER</button>   <!-- Rendre le bouton cliquable -->
     <p>© 2025 Les Quenouilless</p>
   </div>
@@ -30,21 +30,20 @@
 
   h1 {
     font-family: 'Press Start 2P';
-    font-size: 80px;
+    font-size: 15vw;
     color: #384C3F;
     text-align: center;
     margin: 0;
-    padding: 20vh;
-    padding-bottom: 20px;
+    padding: 15vw 5vw 10vh 5vw;
   }
 
   h2 {
     font-family: 'Press Start 2P';
-    font-size: 40px;
+    font-size: 5vw;
     color: #384C3F;
     text-align: center;
     margin: 0;
-    padding: 10vh;
+    padding: 5vw;
   }
 
   .line {
@@ -63,7 +62,7 @@
     border-style: solid;
     border-width: 8px;
     color: #384C3F;
-    font-size: 40px;
+    font-size: 5vw;
     cursor: pointer;
   }
 
@@ -75,10 +74,49 @@
     font-family: 'Press Start 2P';
     color: #384C3F;
     background-color: black;
-    font-size: 20px;
+    font-size: 3vw;
     position: fixed;   
     bottom: 0;          
     left: 0;
     width: 100%;
   }
+
+  @media (min-width: 768px) {
+  h1 {
+    font-size: 80px;         
+    padding: 20vh 0 20px 0; 
+  }
+
+  h2 {
+    font-size: 40px;        
+    padding: 10vh 0;
+  }
+
+  .btnJouer {
+    font-size: 40px;
+    width: 300px;
+    height: 100px;
+  }
+
+  p {
+    font-size: 20px;
+  }
+}
+
+/* --- LARGE DESKTOP --- */
+@media (min-width: 1200px) {
+  h1 {
+    font-size: 95px;    
+  }
+
+  h2 {
+    font-size: 48px;
+  }
+
+  .btnJouer {
+    font-size: 45px;
+    width: 340px;
+    height: 110px;
+  }
+}
 </style>
