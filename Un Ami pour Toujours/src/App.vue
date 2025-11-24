67c754e7ad7d624e713c6356a7c1b1ff519c54b6
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-11-24 15:14:30 -0500

    import GSAP

--- Un Ami pour Toujours/src/App.vue
+++ Un Ami pour Toujours/src/App.vue
@@ -3,6 +3,54 @@
 export default{
   name: 'App', // Nom du composant racine de l'application
 };
+
+import gsap from 'gsap';
+import TextPlugin from 'gsap/TextPlugin';
+
+gsap.registerPlugin(TextPlugin);
+
+export default {
+  methods: {
+    // Code du prof
+    onEnter(el, done) {
+      gsap.from(el, {
+        opacity: 0,
+        y: 30,
+        duration: 0.5,
+        ease: 'power2.out',
+      });
+
+      //////////////  ANIMATION TYPEWRITER  //////////////
+      const paragraph = el.querySelector('p');// on cherche le <p> dans la page
+
+      if (paragraph) {
+        const text = paragraph.textContent;
+        paragraph.textContent = "";  // vide le texte pour commencer l’effet
+
+        gsap.to(paragraph, {
+          duration: text.length * 0.03,
+          text: text,
+          ease: "none",
+          onComplete: done
+        });
+      } else {
+        done();
+      }
+    },
+
+    onLeave(el, done) {
+      gsap.to(el, {
+        opacity: 0,
+        y: -30,
+        duration: 0.3,
+        ease: 'power2.in',
+        onComplete: done
+      });
+    }
+  }
+};
+
+
 </script>
 
 <template>
