a77cd2806d045c1dda4e2de992152bcdc25f3a8b
Felix-Lavoie <aragonaze@gmail.com>
2025-11-24 14:58:29 -0500

    setup de ending view.vue pour test des jeux

--- Un Ami pour Toujours/src/views/EndingScreenView.vue
+++ Un Ami pour Toujours/src/views/EndingScreenView.vue
@@ -1,14 +1,74 @@
 <template>
-  <div class="page"></div>
+  <div class="page">
+    <div>
+      <div class="icone poubelle">
+        <IconePoubelle />
+      </div>
+      <div class="icone ordi">
+        <IconeOrdi />
+      </div>
+      <div class="icone doc">
+        <IconeDoc />
+      </div>
+    </div>
+    <div>
+      <FestinDeMouche/>
+    </div>
+  </div>
+  <AppFooter />
 </template>
 
 <script>
+  import AppFooter from "../components/layout/AppFooter.vue";
+  import IconePoubelle from "../components/specific/IconePoubelle.vue";
+  import IconeOrdi from "../components/specific/IconeOrdi.vue";
+  import IconeDoc from "../components/specific/IconeDoc.vue";
 
+  import FestinDeMouche from "../components/specific/FestinDeMouche.vue";
+  import GrenouilleGlam from "../components/specific/GrenouilleGlam.vue";
+
+  export default {
+    components: {
+      AppFooter,
+      IconeDoc,
+      IconeOrdi,
+      IconePoubelle,
+      FestinDeMouche,
+      GrenouilleGlam
+    }
+  }
 </script>
 
 <style scoped>
 .page {
   height: 100vh;
   background-color: black;
+  background-color: #92a48d;
+}
+
+.icone {
+  padding: 10px;
+  width: 100px;
+  text-align: center;
+  display: flex;
+  position: absolute;
+  z-index: 20;
+}
+
+.poubelle {
+  top: 20px;
+}
+
+.ordi {
+  top: 160px;
+}
+
+.doc {
+  top: 300px;
+}
+
+p {
+  color: white;
+  margin: 0;
 }
 </style>
