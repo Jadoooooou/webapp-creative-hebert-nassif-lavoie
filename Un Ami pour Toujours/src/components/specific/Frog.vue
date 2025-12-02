3f36f7e9f3179d05aa69d8f9e63be5fcf7c7b5a3
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-12-02 12:46:14 -0500

    animation grenouille

--- Un Ami pour Toujours/src/components/specific/Frog.vue
+++ Un Ami pour Toujours/src/components/specific/Frog.vue
@@ -1,18 +1,47 @@
 <template>
-    <img class="frog" src="/src/assets/frog.png" :class="'frog-chapter-' + chapterId"/>
+    <img class="frog" :src="currentFrame" :class="'frog-chapter-' + chapterId" />
 </template>
 
 <script>
+import frogOpen from "../../assets/frog.png";            
+import frogBlink from "../../assets/frog_blink.png";             
+
 export default {
     name: "Frog",
+
     props: {
-        chapterId: {
-            type: Number,
-            required: true
+        chapterId: { type: Number, required: true },
+        talking: { type: Boolean, default: false }
+    },
+
+    data() {
+        return {
+            currentFrame: frogOpen,
+            blinkInterval: null,
+        };
+    },
+
+    mounted() {
+        this.startBlinking();
+    },
+
+
+    methods: {
+        /* Cliquement (automatique)*/
+        startBlinking() {
+            this.blinkInterval = setInterval(() => {
+                this.currentFrame = frogBlink;
+                setTimeout(() => {
+                    this.currentFrame = frogOpen;
+                }, 120);
+            }, 2500 + Math.random() * 2500);
         }
+    },
+
+    beforeUnmount() {
+        clearInterval(this.blinkInterval);
     }
 };
 </script>
 
 <style src="../../assets/styles/frog.css"></style>
-
